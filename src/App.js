6566cc9579e143c9dfb534fedf5874e6900a13e9
Himanshu <himanshusinghadv@gmail.com>
2022-03-25 20:33:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+//import styles
+import "./styles/app.scss";
+//import components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+
+//import data
+import data from "./data";
 
 function App() {
+  //Ref
+  const audioRef = useRef(null);
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const timeUpdateHandler = (event) => {
+    const current = event.target.currentTime;
+    const duration = event.target.duration;
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration: duration,
+      animationPercentage: animation,
+    });
+  };
+
+  const songEndedHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+
+    if (isPlaying) audioRef.current.play();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={` App ${libraryStatus ? "library-active" : ""} `}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+        setSongs={setSongs}
+        //setCurrentSong={setCurrentSong}
+      />
+      <Library
+        audioRef={audioRef}
+        setCurrentSong={setCurrentSong}
+        songs={songs}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={songEndedHandler}
+      ></audio>
     </div>
   );
 }
